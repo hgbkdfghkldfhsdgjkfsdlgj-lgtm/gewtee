1d0b90a1cd407bf9fa26d795074ff1a722b1811e
hgbkdfghkldfhsdgjkfsdlgj-lgtm <hgbkdfghkldfhsdgjkfsdlgj@gmail.com>
2026-04-07 12:06:34 +0200

    aa

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node"
+  testEnvironment: "node",
+  reporters: [
+    "default", // keep default console output
+    [ "jest-junit", {
+        outputDirectory: "./",
+        outputName: "junit.xml"
+      }]
+  ],
+  testMatch: ["**/tests/**/*.test.ts"]
 };
